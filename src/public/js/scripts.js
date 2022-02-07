30ccc85d742c3b528314c25cdc7020e6344babea
charly-zan <54484352+charly-zan@users.noreply.github.com>
2022-02-06 20:38:11 -0600

    paises completos y audios
    se agregaron los audios y paises completos y imágenes

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,27 +1,395 @@
+//Audio index
 var audio = document.getElementById("audiologin");              //codigo del audio
 audio.play();
 
+/////////////////JALISCO////////////
 function jalisco() {
     var audio = document.getElementById("audioJalisco");              //codigo del audio
     audio.play();
+    document.getElementById("mapMex").src="img/mapaMexJal.png";
 }
+function jaliscoLeave(){
+    var audio = document.getElementById("audioJalisco");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+/////////////////SONORA////////////
 function sonora() {
     var audio = document.getElementById("audioSonora");              //codigo del audio
     audio.play();
+    document.getElementById("mapMex").src="img/mapaMexSo.png";
 }
+function sonoraLeave(){
+    var audio = document.getElementById("audioSonora");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+
+/////////////////CHIHUAHUA////////////
 function chihuahua() {
     var audio = document.getElementById("audioChihuahua");              //codigo del audio
     audio.play();
+    document.getElementById("mapMex").src="img/mapaMexChi.png";
 }
+function chihuahuaLeave(){
+    var audio = document.getElementById("audioChihuahua");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+
+/////////////////DURANGO////////////
 function durango() {
     var audio = document.getElementById("audioDurango");              //codigo del audio
     audio.play();
+    document.getElementById("mapMex").src="img/mapaMexDur.png";
 }
+function durangoLeave(){
+    var audio = document.getElementById("audioDurango");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+/////////////////BAJACALIFORNIA////////////
 function bajaCalifornia() {
     var audio = document.getElementById("audioBajaCalifornia");              //codigo del audio
     audio.play();
+    document.getElementById("mapMex").src="img/mapaMexBC.png";
+    
+}
+function bajaCaliforniaLeave(){
+    var audio = document.getElementById("audioBajaCalifornia");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+    
+}
+/////////////////BAJACALIFORNIASUR////////////
+function bajaCaliforniaSurLeave(){
+    var audio = document.getElementById("audioBajaCaliforniaSur");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
 }
 function bajaCaliforniaSur() {
     var audio = document.getElementById("audioBajaCaliforniaSur");              //codigo del audio
     audio.play();
+    document.getElementById("mapMex").src="img/mapaMexBCS.png";
+}
+
+/////////////////SINALOA////////////
+function sinaloaLeave(){
+    var audio = document.getElementById("audioSinaloa");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function sinaloa() {
+    var audio = document.getElementById("audioSinaloa");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexSin.png";
+}
+/////////////////COAHUILA////////////
+function coahuilaLeave(){
+    var audio = document.getElementById("audioCoahuila");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function coahuila() {
+    var audio = document.getElementById("audioCoahuila");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexCoah.png";
+}
+/////////////////NAYARIT////////////
+function nayaritLeave(){
+    var audio = document.getElementById("audioNayarit");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function nayarit() {
+    var audio = document.getElementById("audioNayarit");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexNay.png";
+}
+/////////////////COLIMA////////////
+function colimaLeave(){
+    var audio = document.getElementById("audioColima");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function colima() {
+    var audio = document.getElementById("audioColima");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexCol.png";
+}
+/////////////////zacatecas////////////
+function zacatecasLeave(){
+    var audio = document.getElementById("audioZacatecas");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function zacatecas() {
+    var audio = document.getElementById("audioZacatecas");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexZac.png";
+}
+/////////////////Aguascalientes////////////
+function aguascalientesLeave(){
+    var audio = document.getElementById("audioAguascalientes");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function aguascalientes() {
+    var audio = document.getElementById("audioAguascalientes");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexAgua.png";
+}
+
+/////////////////NuevoLeo////////////
+function nuevoleonLeave(){
+    var audio = document.getElementById("audioNuevoLeon");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function nuevoleon() {
+    var audio = document.getElementById("audioNuevoLeon");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexNL.png";
+}
+/////////////////tamaulipas////////////
+function tamaulipasLeave(){
+    var audio = document.getElementById("audioTamaulipas");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function tamaulipas() {
+    var audio = document.getElementById("audioTamaulipas");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexTam.png";
+}
+/////////////////Michoacan////////////
+function michoacanLeave(){
+    var audio = document.getElementById("audioMichoacan");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function michoacan() {
+    var audio = document.getElementById("audioMichoacan");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexMich.png";
+}
+
+/////////////////Guerrero////////////
+function guerreroLeave(){
+    var audio = document.getElementById("audioGuerrero");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function guerrero() {
+    var audio = document.getElementById("audioGuerrero");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexGue.png";
+}
+/////////////////oaxaca////////////
+function oaxacaLeave(){
+    var audio = document.getElementById("audioOaxaca");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function oaxaca() {
+    var audio = document.getElementById("audioOaxaca");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexOax.png";
+}
+/////////////////chiapas////////////
+function chiapasLeave(){
+    var audio = document.getElementById("audioChiapas");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function chiapas() {
+    var audio = document.getElementById("audioChiapas");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexChiap.png";
+}
+/////////////////yucatan////////////
+function yucatanLeave(){
+    var audio = document.getElementById("audioYucatan");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function yucatan() {
+    var audio = document.getElementById("audioYucatan");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexYuc.png";
+}
+/////////////////QuintanaRoo////////////
+function quintanaRooLeave(){
+    var audio = document.getElementById("audioQuintanaRoo");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function quintanaRoo() {
+    var audio = document.getElementById("audioQuintanaRoo");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexQRoo.png";
+}
+/////////////////campeche////////////
+function campecheLeave(){
+    var audio = document.getElementById("audioCampeche");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function campeche() {
+    var audio = document.getElementById("audioCampeche");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexCam.png";
+}
+/////////////////tabasco////////////
+function tabascoLeave(){
+    var audio = document.getElementById("audioTabasco");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function tabasco() {
+    var audio = document.getElementById("audioTabasco");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexTab.png";
+}
+/////////////////veracruz////////////
+function veracruzLeave(){
+    var audio = document.getElementById("audioVeracruz");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function veracruz() {
+    var audio = document.getElementById("audioVeracruz");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexVer.png";
+}
+/////////////////puebla////////////
+function pueblaLeave(){
+    var audio = document.getElementById("audioPuebla");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function puebla() {
+    var audio = document.getElementById("audioPuebla");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexPue.png";
+}
+/////////////////sanLuisPotosi////////////
+function sanLuisPotosiLeave(){
+    var audio = document.getElementById("audioSanLuisPotosi");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function sanLuisPotosi() {
+    var audio = document.getElementById("audioSanLuisPotosi");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexSLP.png";
+}
+/////////////////guanajuato////////////
+function guanajuatoLeave(){
+    var audio = document.getElementById("audioGuanajuato");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function guanajuato() {
+    var audio = document.getElementById("audioGuanajuato");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexGua.png";
+}
+/////////////////mexico////////////
+function mexicoLeave(){
+    var audio = document.getElementById("audioMexico");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function mexico() {
+    var audio = document.getElementById("audioMexico");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexMex.png";
+}
+/////////////////queretaro////////////
+function queretaroLeave(){
+    var audio = document.getElementById("audioQueretaro");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function queretaro() {
+    var audio = document.getElementById("audioQueretaro");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexQue.png";
+}
+/////////////////hidalgo////////////
+function hidalgoLeave(){
+    var audio = document.getElementById("audioHidalgo");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function hidalgo() {
+    var audio = document.getElementById("audioHidalgo");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMeHid.png";
+}
+/////////////////morelos////////////
+function morelosLeave(){
+    var audio = document.getElementById("audioMorelos");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function morelos() {
+    var audio = document.getElementById("audioMorelos");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexMor.png";
+}
+/////////////////cdmx////////////
+function cdmxLeave(){
+    var audio = document.getElementById("audioCDMX");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function cdmx() {
+    var audio = document.getElementById("audioCDMX");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexCDMX.png";
+}
+/////////////////tlaxcala////////////
+function tlaxcalaLeave(){
+    var audio = document.getElementById("audioTlaxcala");              //codigo del audio
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById("mapMex").src="img/mapaMexpp2.png";
+}
+function tlaxcala() {
+    var audio = document.getElementById("audioTlaxcala");              //codigo del audio
+    audio.play();
+    document.getElementById("mapMex").src="img/mapaMexTla.png";
 }
